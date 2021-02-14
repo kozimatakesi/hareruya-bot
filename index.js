18f2ea61ed5b93cb37cf90f7a7219ebca0f2e3e8
riverbook708 <riverbook708@gmail.com>
2021-02-14 13:19:00 +0900

    add fetchAPI

--- index.js
+++ index.js
@@ -35,9 +35,8 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
         if (event.type == "message" && event.message.type == "text"){
             // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
             if (event.message.text == "こんにちは"){
-              var ress = await fetch("http://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=9a4d371b6fc452d3edd2f79b142c8c18&lang=ja&units=metric");
-              var resp = ress.json();
-              var nowWeather = `現在は${resp.weather[0].description}`;
+              fetch('http://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=9a4d371b6fc452d3edd2f79b142c8c18&lang=ja&units=metric').then(res => res.json()).then(json => console.log(json));
+              const nowWeather = res..weather[0].description;
               // replyMessage()で返信し、そのプロミスをevents_processedに追加。
                 events_processed.push(bot.replyMessage(event.replyToken, {
                     type: "text",
