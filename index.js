5e0ed8aad73b833044bdab1d619e593a505d1f41
riverbook708 <riverbook708@gmail.com>
2021-02-20 12:37:52 +0900

    add puppetter option

--- index.js
+++ index.js
@@ -105,7 +105,9 @@ server.post('/webhook', line.middleware(lineConfig), (req, res) => {
       if(event.message.text === 'プペッター'){
         !(async() => {
           try {
-            const browser = await puppetter.launch();
+            const browser = await puppetter.launch({
+              ignoreDefaultArgs: ['--disable-extensions'],
+            });
             const page = await browser.newPage();
             await page.goto('https://www.hareruyamtg.com/ja/products/search?cardset=242&rarity%5B0%5D=4&rarity%5B1%5D=3&foilFlg%5B0%5D=0&sort=price&order=DESC&page=1');
             //datasにitemNameの値を全て取得後、配列にして代入
