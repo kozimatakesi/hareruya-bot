f1edcca2cf36cb8bf3279496d79dbd0d8a95ad53
riverbook708 <riverbook708@gmail.com>
2021-03-04 19:43:19 +0900

    local add lint

--- index.js
+++ index.js
@@ -3,19 +3,16 @@ const server = require('express')();
 const line = require('@line/bot-sdk'); // Messaging APIのSDKをインポート
 const fetch = require('node-fetch');
 const mysql = require('mysql');
-const jsdom = require('jsdom');
 const puppetter = require('puppeteer');
-const { JSDOM } = jsdom;
+
 // -----------------------------------------------------------------------------
 // データベース接続
-/*
 const pool = mysql.createPool({
   host: process.env.SERVER_NAME,
   user: process.env.USER_NAME,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
 });
-*/
 // -----------------------------------------------------------------------------
 // パラメータ設定
 /*
@@ -25,8 +22,8 @@ const lineConfig = {
 };
 */
 const lineConfig = {
-  channelAccessToken:'w6VjWFqXigMZhPTwkqq5aQZBd563o04eEvUNQTKTUp87LGfliqD0O5BPl7431xeZeWwU2OJlvfo7/TOpoWnFW2NhjqcYNK5AG9rcqEvF9hoTM+6/JuCWYxnRnVkKn2jq1Ua8q2E/qbN8mcQtssAViQdB04t89/1O/w1cDnyilFU=', // 環境変数からアクセストークンをセットしています
-  channelSecret:'98cf4cabb5c2bcd86b00de14fb8814cd', // 環境変数からChannel Secretをセットしています
+  channelAccessToken: 'w6VjWFqXigMZhPTwkqq5aQZBd563o04eEvUNQTKTUp87LGfliqD0O5BPl7431xeZeWwU2OJlvfo7/TOpoWnFW2NhjqcYNK5AG9rcqEvF9hoTM+6/JuCWYxnRnVkKn2jq1Ua8q2E/qbN8mcQtssAViQdB04t89/1O/w1cDnyilFU=', // 環境変数からアクセストークンをセットしています
+  channelSecret: '98cf4cabb5c2bcd86b00de14fb8814cd', // 環境変数からChannel Secretをセットしています
 };
 // -----------------------------------------------------------------------------
 // Webサーバー設定
@@ -48,12 +45,6 @@ server.post('/webhook', line.middleware(lineConfig), (req, res) => {
 
   // イベントオブジェクトを順次処理。
   req.body.events.forEach((event) => {
-    if (event.type === 'sticker') {
-      bot.pushMessage('U6b3963a1368a4879d411264a6950a01d', {
-        type: 'text',
-        text: 'スタンプを送るな！',
-      });
-    }
     // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
     if (event.type === 'message' && event.message.type === 'text') {
       // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
@@ -74,7 +65,7 @@ server.post('/webhook', line.middleware(lineConfig), (req, res) => {
         }));
         bot.pushMessage('U6b3963a1368a4879d411264a6950a01d', {
           type: 'text',
-          text: 'そうとはいうけれども、たださぁ、そんなでもないよ？',
+          text: 'そうとはいうけれども',
         });
       }
       if (event.message.text === '読み込む') {
@@ -92,9 +83,8 @@ server.post('/webhook', line.middleware(lineConfig), (req, res) => {
           );
         });
       }
-      /*
-      //「！」を入力したあとの文字と価格[2000]をデータベースに書き込む
-      if (event.message.text.match('!')) {
+      // 「?」を入力したあとの文字と価格[2000]をデータベースに書き込む
+      if (event.message.text.match('¥')) {
         const inputMessage = event.message.text.slice(1);
         pool.getConnection((err, connection) => {
           connection.query(
@@ -110,7 +100,6 @@ server.post('/webhook', line.middleware(lineConfig), (req, res) => {
           );
         });
       }
-      */
 
       if (event.message.text.match('!')) {
         bot.pushMessage('U6b3963a1368a4879d411264a6950a01d', {
@@ -118,6 +107,7 @@ server.post('/webhook', line.middleware(lineConfig), (req, res) => {
           text: 'データ取得中、しばらくお待ちください',
         });
         const inputMessage = event.message.text.slice(1);
+
         !(async () => {
           try {
             const browser = await puppetter.launch({
@@ -126,25 +116,43 @@ server.post('/webhook', line.middleware(lineConfig), (req, res) => {
             const page = await browser.newPage();
             await page.goto('https://www.hareruyamtg.com/ja/products/search');
 
+            if (inputMessage === '') {
+              bot.pushMessage('U6b3963a1368a4879d411264a6950a01d', {
+                type: 'text',
+                text: '見当たりませんでした',
+              });
+              browser.close();
+              return;
+            }
+
             const datas = await page.evaluate(() => {
               const list = [...document.querySelectorAll('#front_product_search_cardset option')];
-              return list.map(data => ({name: data.textContent, value: data.value}));
+              return list.map((data) => ({ name: data.textContent, value: data.value }));
             });
 
             const nameArray = [];
-            for(let i = 0; i < datas.length; i++){
-              if(datas[i].name.match(inputMessage)) {
+            for (let i = 0; i < datas.length; i++) {
+              if (datas[i].name.match(inputMessage)) {
                 nameArray.push(datas[i]);
               }
             }
             console.log(nameArray);
 
-            let manyExpantion = [];
-            if(nameArray.length > 1){
-              for(let j = 0; j < nameArray.length; j++){
-                  manyExpantion.push(`「${nameArray[j].name}」は「${nameArray[j].value}」です`)
+            if (nameArray.length === 0) {
+              bot.pushMessage('U6b3963a1368a4879d411264a6950a01d', {
+                type: 'text',
+                text: '見当たりませんでした',
+              });
+              browser.close();
+              return;
+            }
+
+            const manyExpantion = [];
+            if (nameArray.length > 1) {
+              for (let j = 0; j < nameArray.length; j++) {
+                manyExpantion.push(`「${nameArray[j].name}」は「${nameArray[j].value}」です`);
               }
-              const joinArray = manyExpantion.join(`\n---------------------------------------------\n`);
+              const joinArray = manyExpantion.join('\n---------------------------------------------\n');
               console.log(joinArray);
               bot.pushMessage('U6b3963a1368a4879d411264a6950a01d', {
                 type: 'text',
@@ -152,7 +160,7 @@ server.post('/webhook', line.middleware(lineConfig), (req, res) => {
               });
               bot.pushMessage('U6b3963a1368a4879d411264a6950a01d', {
                 type: 'text',
-                text: `複数一致しています、知りたいエキスパンションのナンバーを入力してください`,
+                text: '複数一致しています、知りたいエキスパンションのナンバーを入力してください',
               });
             } else {
               bot.pushMessage('U6b3963a1368a4879d411264a6950a01d', {
@@ -166,15 +174,15 @@ server.post('/webhook', line.middleware(lineConfig), (req, res) => {
                   });
                   const page = await browser.newPage();
                   await page.goto(`https://www.hareruyamtg.com/ja/purchase/search?cardset=${nameArray[0].value}&rarity%5B0%5D=4&rarity%5B1%5D=3&foilFlg%5B0%5D=0&purchaseFlg=1&sort=price&order=DESC&page=1`);
-                  //datasにitemNameの値を全て取得後、配列にして代入
+                  // datasにitemNameの値を全て取得後、配列にして代入
                   const datas = await page.evaluate(() => {
                     const list = [...document.querySelectorAll('.itemName')];
-                    return list.map(data => data.textContent.trim());
+                    return list.map((data) => data.textContent.trim());
                   });
-                  //pricesにitemDetail__priceの値を全て取得後、配列にして代入
+                  // pricesにitemDetail__priceの値を全て取得後、配列にして代入
                   const prices = await page.evaluate(() => {
                     const list = [...document.querySelectorAll('.itemDetail__price')];
-                    return list.map(data => data.textContent);
+                    return list.map((data) => data.textContent);
                   });
 
                   let i = 0;
@@ -184,27 +192,27 @@ server.post('/webhook', line.middleware(lineConfig), (req, res) => {
                       text: `第${i + 1}位\n${datas[i]}\n${prices[i]}`,
                     });
                     console.log(i++);
-                  }
+                  };
                   const intervalId = setInterval(() => {
                     countUp();
-                    if(i >= 5){
+                    if (i >= 5) {
                       clearInterval(intervalId);
                     }
-                  },500);
+                  }, 500);
                   browser.close();
-                } catch(e) {
+                } catch (e) {
                   console.error(e);
                 }
               })();
             }
             browser.close();
-          } catch(e) {
+          } catch (e) {
             console.error(e);
           }
         })();
       }
 
-      if(event.message.text.match(/[0-9]{1,3}/)){
+      if (event.message.text.match(/[0-9]{1,3}/)) {
         bot.pushMessage('U6b3963a1368a4879d411264a6950a01d', {
           type: 'text',
           text: 'データ取得中、しばらくお待ちください',
@@ -217,29 +225,29 @@ server.post('/webhook', line.middleware(lineConfig), (req, res) => {
             const page = await browser.newPage();
             await page.goto(`https://www.hareruyamtg.com/ja/purchase/search?cardset=${event.message.text}&rarity%5B0%5D=4&rarity%5B1%5D=3&foilFlg%5B0%5D=0&purchaseFlg=1&sort=price&order=DESC&page=1`);
 
-            //エキスパンション名の取得
+            // エキスパンション名の取得
             const expName = await page.evaluate(() => {
               const list = [...document.querySelectorAll('#front_product_search_cardset option')];
-              return list.map(data => ({name: data.textContent, value: data.value}));
+              return list.map((data) => ({ name: data.textContent, value: data.value }));
             });
 
-            for(let i = 0; i < expName.length; i++){
-              if(expName[i].value.match(event.message.text)){
+            for (let i = 0; i < expName.length; i++) {
+              if (expName[i].value === event.message.text) {
                 bot.pushMessage('U6b3963a1368a4879d411264a6950a01d', {
                   type: 'text',
                   text: `エキスパンション:${expName[i].name}`,
                 });
-                }
+              }
             }
-            //datasにitemNameの値を全て取得後、配列にして代入
+            // datasにitemNameの値を全て取得後、配列にして代入
             const datas = await page.evaluate(() => {
               const list = [...document.querySelectorAll('.itemName')];
-              return list.map(data => data.textContent.trim());
+              return list.map((data) => data.textContent.trim());
             });
-            //pricesにitemDetail__priceの値を全て取得後、配列にして代入
+            // pricesにitemDetail__priceの値を全て取得後、配列にして代入
             const prices = await page.evaluate(() => {
               const list = [...document.querySelectorAll('.itemDetail__price')];
-              return list.map(data => data.textContent);
+              return list.map((data) => data.textContent);
             });
 
             let i = 0;
@@ -249,21 +257,21 @@ server.post('/webhook', line.middleware(lineConfig), (req, res) => {
                 text: `第${i + 1}位\n${datas[i]}\n${prices[i]}`,
               });
               console.log(i++);
-            }
+            };
             const intervalId = setInterval(() => {
               countUp();
-              if(i >= 5){
+              if (i >= 5) {
                 clearInterval(intervalId);
               }
-            },500);
+            }, 500);
             browser.close();
-          } catch(e) {
+          } catch (e) {
             console.error(e);
           }
         })();
       }
 
-      if(event.message.text === 'プペッター'){
+      if (event.message.text === 'プペッター') {
         !(async () => {
           try {
             const browser = await puppetter.launch({
@@ -271,16 +279,16 @@ server.post('/webhook', line.middleware(lineConfig), (req, res) => {
             });
             const page = await browser.newPage();
             await page.goto('https://www.hareruyamtg.com/ja/products/search?cardset=242&rarity%5B0%5D=4&rarity%5B1%5D=3&foilFlg%5B0%5D=0&sort=price&order=DESC&page=1');
-            //datasにitemNameの値を全て取得後、配列にして代入
+            // datasにitemNameの値を全て取得後、配列にして代入
             const datas = await page.evaluate(() => {
               const list = [...document.querySelectorAll('.itemName')];
-              return list.map(data => data.textContent.trim());
+              return list.map((data) => data.textContent.trim());
             });
 
-            //pricesにitemDetail__priceの値を全て取得後、配列にして代入
+            // pricesにitemDetail__priceの値を全て取得後、配列にして代入
             const prices = await page.evaluate(() => {
               const list = [...document.querySelectorAll('.itemDetail__price')];
-              return list.map(data => data.textContent);
+              return list.map((data) => data.textContent);
             });
 
             bot.pushMessage('U6b3963a1368a4879d411264a6950a01d', {
@@ -294,49 +302,46 @@ server.post('/webhook', line.middleware(lineConfig), (req, res) => {
             });
 
             browser.close();
-          } catch(e) {
+          } catch (e) {
             console.error(e);
           }
         })();
       }
     }
   });
-  /*
   // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
   Promise.all(eventsProcessed).then(
     (response) => {
       console.log(`${response.length} event(s) processed.`);
     },
   );
-  */
-
-
 });
 // -----------------------------------------------------------------------------
+/*
+const hogehoge = async (input) => {
+  try {
+    const browser = await puppetter.launch({
+      args: ['--no-sandbox'],
+    });
+    const page = await browser.newPage();
+    // 晴れる屋のサイトに遷移
+    await page.goto('https://www.hareruyamtg.com/ja/products/search');
 
-  const hogehoge = async(input) => {
-    try {
-      const browser = await puppetter.launch({
-        args: ['--no-sandbox'],
-      });
-      const page = await browser.newPage();
-      //晴れる屋のサイトに遷移
-      await page.goto('https://www.hareruyamtg.com/ja/products/search');
-
-      //datasにitemNameの値を全て取得後、配列にして代入
-      const datas = await page.evaluate(() => {
-        const list = [...document.querySelectorAll('#front_product_search_cardset option')];
-        return list.map(data => ({name: data.textContent, value: data.value }));
-      });
-      //上位５枚のカード名と値段を表示
-      const nameArray = [];
-      for(let i = 0; i < datas.length; i++){
-        if(datas[i].name.match(input)) {
-          nameArray.push(datas[i]);
-        }
+    // datasにitemNameの値を全て取得後、配列にして代入
+    const datas = await page.evaluate(() => {
+      const list = [...document.querySelectorAll('#front_product_search_cardset option')];
+      return list.map((data) => ({ name: data.textContent, value: data.value }));
+    });
+      // 上位５枚のカード名と値段を表示
+    const nameArray = [];
+    for (let i = 0; i < datas.length; i++) {
+      if (datas[i].name.match(input)) {
+        nameArray.push(datas[i]);
       }
-      browser.close();
-    } catch(e) {
-      console.error(e);
     }
+    browser.close();
+  } catch (e) {
+    console.error(e);
   }
+};
+*/
